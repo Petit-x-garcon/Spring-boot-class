71f38a22244c6f88839c34b3015f96c46c19f157
Petit-x-garcon <sambat.yin168@gmail.com>
2025-08-07 11:23:34 +0700

    feature: add validation Exception handler

--- demo/src/main/java/com/sambat/demo/Exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/sambat/demo/Exception/GlobalExceptionHandler.java
@@ -2,11 +2,17 @@ package com.sambat.demo.Exception;
 
 import com.sambat.demo.Exception.Model.DuplicatedException;
 import com.sambat.demo.Exception.Model.NotFoundHandler;
+import com.sambat.demo.Model.BaseDataResponseModel;
 import com.sambat.demo.Model.BaseResponseModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,7 +28,21 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<BaseResponseModel> handleInternal(Exception ex){
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new BaseResponseModel("fail", "internal server error" + ex.getMessage()));
+    public ResponseEntity<BaseResponseModel> handleGenernalException(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new BaseResponseModel("fail", "internal server error " + ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseDataResponseModel> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String field = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+
+            errors.put(field, message);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BaseDataResponseModel("fail", "validation fail", errors));
     }
 }
