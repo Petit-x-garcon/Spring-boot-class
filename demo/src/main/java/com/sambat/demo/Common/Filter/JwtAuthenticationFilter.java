31fbc0f5bed4eca7f196e9ed63ac3d37fbfc444c
Petit-x-garcon <sambat.yin168@gmail.com>
2025-09-11 12:05:06 +0700

    refactor(auth): return value if authenticated

--- demo/src/main/java/com/sambat/demo/Common/Filter/JwtAuthenticationFilter.java
+++ demo/src/main/java/com/sambat/demo/Common/Filter/JwtAuthenticationFilter.java
@@ -1,9 +1,16 @@
 package com.sambat.demo.Common.Filter;
 
+import com.sambat.demo.Service.UserService;
+import com.sambat.demo.Service.security.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,6 +18,11 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private UserService userService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
@@ -24,11 +36,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             jwtToken = requestTokenHeader.substring(7);
 
             try{
-                username = " ";
+                username = jwtUtil.extractUsername(jwtToken);
             }catch (Exception e){
                 throw e;
             }
         }
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userService.loadUserByUsername(username);
+
+            if(jwtUtil.validateToken(jwtToken, userDetails)){
+                UsernamePasswordAuthenticationToken userAuthToken =
+            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                userAuthToken.setDetails(new WebAuthenticationDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(userAuthToken);
+            }
+        }
+
+
         filterChain.doFilter(request,response);
     }
 }
--- demo/src/main/java/com/sambat/demo/Service/security/JwtUtil.java
+++ demo/src/main/java/com/sambat/demo/Service/security/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.sambat.demo.Service.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -11,6 +12,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -41,5 +43,33 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + this.expiration))
                 .signWith(this.getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails){
+        boolean isExpired = this.extractExpiration(token).before(new Date());
+        String username = this.extractUsername(token);
+        return !isExpired && username.equals(userDetails.getUsername());
+    }
+
+    public Date extractExpiration(String token){
+        return this.extractClaim(token, Claims::getExpiration);
+    }
+
+    public String extractUsername(String token){
+        return this.extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver){
+        Claims claims = this.extractAllClaims(token);
+
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(this.getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
